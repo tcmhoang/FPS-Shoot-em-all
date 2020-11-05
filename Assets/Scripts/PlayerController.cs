24d679431ac40dd64253bcdb5c5b7f779f5a528d
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-11-05 02:46:24 -0500

    Add running and camera motions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ namespace Assets.Scripts
 {
     public class PlayerController : MonoBehaviour
     {
-        public float MoveSpeed, GravityModifier, JumpPower;
+        public float MoveSpeed, GravityModifier, JumpPower, RunSpeed = 12f;
 
         private float _baseGravity;
 
@@ -19,9 +19,12 @@ namespace Assets.Scripts
         public bool InvertX;
         public bool InvertY;
 
-        private bool _jumpable;
         public Transform GroundCheckPoint;
-        public LayerMask WhatIsGround;
+        public LayerMask GroundRadar;
+        private bool _canJump;
+        private bool _canJumpAgain;
+
+        public Animator Animator;
 
         // Start is called before the first frame update
         private void Start()
@@ -39,7 +42,7 @@ namespace Assets.Scripts
             // _moveInput.z = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
             var currentYVelocity = _moveInput.y;
 
-            SetNewLocationToMove();
+            SetMoveVector();
 
             SetGravity(currentYVelocity);
 
@@ -48,21 +51,37 @@ namespace Assets.Scripts
             _characterController.Move(_moveInput * Time.deltaTime);
 
             RotateCameraByCursor();
+
+            Animate();
+        }
+
+        private void Animate()
+        {
+            Animator.SetFloat("MoveSpeed", _moveInput.magnitude);
+            Animator.SetBool("OnGround",_canJump);
         }
 
         private void Jump()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            _canJump = Physics.OverlapSphere(GroundCheckPoint.position, .25f, GroundRadar).Length > 0;
+            if (!Input.GetKeyDown(KeyCode.Space)) return;
+            if (_canJump)
             {
                 _moveInput.y = JumpPower;
+                _canJumpAgain = true;
+            }
+            else if (_canJumpAgain)
+            {
+                _moveInput.y = JumpPower;
+                _canJumpAgain = false;
             }
         }
+
 
         private void RotateCameraByCursor()
         {
             var mouseInVector = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) *
                                 MouseSensitivity;
-
             if (InvertX) mouseInVector.x *= -1;
             if (InvertY) mouseInVector.y *= -1;
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
@@ -71,21 +90,18 @@ namespace Assets.Scripts
                 Quaternion.Euler(CamTransform.rotation.eulerAngles + new Vector3(-mouseInVector.y, 0f, 0f));
         }
 
-        private void SetNewLocationToMove()
+        private void SetMoveVector()
         {
             var verticalMove = transform.forward * Input.GetAxis("Vertical");
             var horizontalMove = transform.right * Input.GetAxis("Horizontal");
-
-            _moveInput = (verticalMove + horizontalMove).normalized * MoveSpeed;
+            _moveInput = (verticalMove + horizontalMove).normalized;
+            _moveInput *= Input.GetKey(KeyCode.LeftShift) ? RunSpeed : MoveSpeed;
         }
 
         private void SetGravity(float currentYVelocity)
         {
-
-            _moveInput.y =_baseGravity;
-            
+            _moveInput.y = _baseGravity;
             _moveInput.y += _characterController.isGrounded ? 0 : currentYVelocity;
-
         }
     }
 }
